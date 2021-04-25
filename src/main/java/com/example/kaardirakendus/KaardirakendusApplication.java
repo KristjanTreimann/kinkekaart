39cd17a2c2dddef6d484832f41fc97f1e287cc26
Kristjan T <43129577+KristjanTreimann@users.noreply.github.com>
2021-04-25 10:07:45 +0300

    Initial working commit, no JPA dependency used

---
+++ src/main/java/com/example/kaardirakendus/KaardirakendusApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaardirakendus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaardirakendusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaardirakendusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kaardirakendus/KaardirakendusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kaardirakendus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaardirakendusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
